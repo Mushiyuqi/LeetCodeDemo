598b80042c29685513f2f8a5d8ee171ba7dd1374
Mushiyuqi <116066490+Mushiyuqi@users.noreply.github.com>
2023-11-15 19:56:59 +0800

    376.摆动序列

--- main.cpp
+++ main.cpp
@@ -14,64 +14,21 @@
 using namespace std;
 
 class Solution {
-
-    bool isOk(vector<vector<char>> &board, int l, int r) {
-        for (int i = 0; i != 9; ++i) {
-            if (i == r)
-                continue;
-            if (board[l][i] == board[l][r])
-                return false;
-        }
-
-        for (int i = 0; i != 9; ++i) {
-            if (i == l)
-                continue;
-            if (board[i][r] == board[l][r])
-                return false;
-        }
-
-        int _l = (l / 3) * 3;
-        int _r = (r / 3) * 3;
-        for (int i = _l; i != _l + 3; ++i) {
-            if (i == l)
-                continue;
-            for (int j = _r; j != _r + 3; ++j) {
-                if (j == r)
-                    continue;
-                if (board[i][j] == board[l][r])
-                    return false;
-            }
-        }
-
-        return true;
-    }
-
-    bool backTrack(vector<vector<char>> &board) {
-
-        for (int i = 0; i != 9; ++i) {
-            for (int j = 0; j != 9; ++j) {
-                if(board[i][j] != '.')
-                    continue;
-
-                for(char m = '1'; m != '9' + 1; ++m){
-                    board[i][j] = m;
-                    if(isOk(board, i, j)){
-                        if(backTrack(board))
-                            return true;
-                    }
-                    board[i][j] = '.';
-                }
-
-                return false;
-            }
-        }
-
-        return true;
-    }
-
 public:
-    void solveSudoku(vector<vector<char>> &board) {
-        backTrack(board);
+    int wiggleMaxLength(vector<int> &&nums) {
+        int len = nums.size();
+        int preDiff = 0;
+        int curDiff = 0;
+        int res = 1;
+
+        for (int i = 0; i < len - 1; ++i) {
+            curDiff = nums[i + 1] - nums[i];
+            if ((preDiff <= 0 && curDiff > 0) || (preDiff >= 0 && curDiff < 0))
+                res++;
+            if (curDiff != 0)
+                preDiff = curDiff;
+        }
+        return res;
     }
 };
 
@@ -79,22 +36,13 @@ int main() {
 
     Solution s;
 
-    vector<vector<char>> board{{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
-                               {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
-                               {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
-                               {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
-                               {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
-                               {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
-                               {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
-                               {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
-                               {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    cout << s.wiggleMaxLength({1, 2, 3, 4, 5, 6, 7, 8, 9}) << endl;
 
-    s.solveSudoku(board);
-    for (auto e: board) {
-        for (auto n: e)
-            cout << n << ' ';
-        cout << endl;
-    }
+//    for (auto e: board) {
+//        for (auto n: e)
+//            cout << n << ' ';
+//        cout << endl;
+//    }
 
 
     return 0;
