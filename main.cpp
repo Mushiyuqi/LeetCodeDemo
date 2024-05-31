e89fedfc7ce0c3c524cf00f11113164959035487
Mushiyuqi <116066490+Mushiyuqi@users.noreply.github.com>
2024-05-31 20:48:17 +0800

    BFS

--- main.cpp
+++ main.cpp
@@ -1,54 +1,59 @@
-#include<iostream>
-#include <string>
-#include <algorithm>
+#include <iostream>
+#include <stack>
 #include <vector>
-#include <set>
-#include <map>
-#include <unordered_map>
-#include <sstream>
-#include <unordered_set>
-#include <cmath>
 #include <queue>
-#include <numeric>
-#include <list>
-#include <array>
-
+#include <map>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
+queue<pair<int, int>> q;
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+int x, y, ans = 0;
 
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-class Solution {
-    int result = 0;
-    int travel(TreeNode *root){
-        if(root == nullptr) return 2;
-        int left = travel(root->left);
-        int right = travel(root->right);
-
-        if(left == 2 && right == 2) return 0;
-        if(left == 0 || right == 0) {++result; return 1;}
-        return 2;
+void bfs(long long sx, long long sy, vector<vector<bool>>& vis, vector<string>& arr){
+    pair<int, int> nt{sx, sy};
+    vis[nt.first][nt.second] = true;
+    q.push(nt);
+    while(!q.empty()){
+        pair<int, int> tmp = q.front();
+        for(int i = 0; i != 4; ++i){
+            int tx = tmp.first + dx[i];
+            int ty = tmp.second + dy[i];
+            if(tx >= 0 && tx < x && ty >= 0 && ty < y && arr[tx][ty] == '1'  && !vis[tx][ty]){
+                vis[tx][ty] = true;
+                q.emplace(tx, ty);
+            }
+        }
+        q.pop();
     }
-
-public:
-    int minCameraCover(TreeNode *root) {
-        if(travel(root) == 0) return ++result;
-        return result;
-    }
-};
+}
 
 int main() {
+    vector<string> arr {
+        "0101",
+        "1001",
+        "0101",
+        "1111"
+    };
+    x = arr.size();
+    y = arr[0].size();
+    vector<vector<bool>> vis(x, vector<bool>(y, false));
 
-    Solution s;
+    for(int i = 0; i != x; ++i){
+        for(int j = 0; j != y; ++j){
+            if(vis[i][j] || arr[i][j] == '0')
+                continue;
+            bfs(i, j, vis, arr);
+            ++ans;
+        }
+    }
 
+    for(const auto& e : arr){
+        for(auto u : e){
+            cout<< u << " ";
+        }
+        cout<<endl;
+    }
+    cout<< ans << endl;
     return 0;
 }
